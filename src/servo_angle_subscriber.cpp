cf2771dfba512c992a6fce525e6ffb179c26f559
curtisburke-eng <curtisburke.eng@gmail.com>
2022-10-18 23:34:12 -0600

    Added logic for second joint

--- src/servo_angle_subscriber.cpp
+++ src/servo_angle_subscriber.cpp
@@ -5,35 +5,54 @@
 #include "PCA9685.h"
 
 // Delare and Define Variables
-#define SERVO_CHANNEL 7
+#define J1_SERVO_CHANNEL 7
+#define J2_SERVO_CHANNEL 8
 PCA9685 pwmController(Wire);
 PCA9685_ServoEval pwmServo1;
+PCA9685_ServoEval pwmServo2;
 
 // Declare ROS backend variable
 ros::NodeHandle node_handle;
 
 // Create a fuction that happens each time an event is triggered (ie a message is recieved on the topic)
-void callback(const std_msgs::UInt16& angle)
+void joint1_cb(const std_msgs::UInt16& angle)
 {
   // Print for debug
-  Serial.print("Angle from msg: ");
-  Serial.println(angle.data);
+  //Serial.print("Angle from msg: ");
+  //Serial.println(angle.data);
   
   // Write the message (angle) to the servo using pwm value
-  pwmController.setChannelPWM(SERVO_CHANNEL, pwmServo1.pwmForAngle(angle.data));
+  pwmController.setChannelPWM(J1_SERVO_CHANNEL, pwmServo1.pwmForAngle(angle.data));
   
   // Print for debug
-  Serial.print("PWM Value from given angle: ");
-  Serial.println(pwmController.getChannelPWM(SERVO_CHANNEL)); 
+  //Serial.print("PWM Value from given angle: ");
+  //Serial.println(pwmController.getChannelPWM(J1_SERVO_CHANNEL)); 
+}
+
+void joint2_cb(const std_msgs::UInt16& angle)
+{
+  // Print for debug
+  //Serial.print("Angle from msg: ");
+  //Serial.println(angle.data);
+  
+  // Write the message (angle) to the servo using pwm value
+  pwmController.setChannelPWM(J2_SERVO_CHANNEL, pwmServo2.pwmForAngle(angle.data));
+  
+  // Print for debug
+  //Serial.print("PWM Value from given angle: ");
+  //Serial.println(pwmController.getChannelPWM(J2_SERVO_CHANNEL)); 
 }
 
 // Set up ROS Subsriber on the topic of joint1_angle_topic
-ros::Subscriber<std_msgs::UInt16> sub("joint1_angle_topic", &callback);
+ros::Subscriber<std_msgs::UInt16> J1_sub("joint1_angle_topic", &joint1_cb);
+
+// Set up ROS Subsriber on the topic of joint2_angle_topic
+ros::Subscriber<std_msgs::UInt16> J2_sub("joint2_angle_topic", &joint2_cb);
 
 // Run the set up parameters once
 void setup()
 {
-  Serial.begin(9600);                 // Begin Serial and Wire interfaces
+  //Serial.begin(9600);                 // Begin Serial and Wire interfaces
   Wire.begin();
 
   pwmController.resetDevices();       // Resets all PCA9685 devices on i2c line
@@ -41,8 +60,9 @@ void setup()
   pwmController.setPWMFreqServo();    // 50Hz provides standard 20ms servo phase length
   
   
-  node_handle.initNode();             // Initialize the conversation with the ROS backend
-  node_handle.subscribe(sub);         // Tell ROS backend that this sub is subscribing to the topic
+  node_handle.initNode();               // Initialize the conversation with the ROS backend
+  node_handle.subscribe(J1_sub);        // Tell ROS backend that this sub is subscribing to the topic for J1
+  node_handle.subscribe(J2_sub);        // Tell ROS backend that this sub is subscribing to the topic for J2
 
 }
 
